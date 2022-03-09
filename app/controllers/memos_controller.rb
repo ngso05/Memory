e73f2386207651f9b45147a8057e5f574330703b
EC2 Default User <ec2-user@ip-172-31-4-83.ap-northeast-1.compute.internal>
2022-03-09 16:18:59 +0000

    add Gemfile

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,7 +1,34 @@
 class MemosController < ApplicationController
   def index
   end
-  
+
   def new
-    
+    @memo = Memo.new
+  end
+
+  def create
+    @memo = Memo.new(memo_params)
+    memo.save
+    if @memo.save
+      redirect_to memos_path
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  private
+  def memo_params
+    params.require(:memo).permit(:title, :body)
+  end
 end
